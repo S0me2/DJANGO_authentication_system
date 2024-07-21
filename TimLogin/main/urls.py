c458a4dc09869704bbbdd7e5b8e5fded530215ec
S0me2 <you@example.com>
2024-07-21 12:33:27 +0500

    3rd commit

--- TimLogin/main/urls.py
+++ TimLogin/main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('home/', views.home, name='home'),
     path('sign-up/', views.sign_up, name='sign_up'),
+    path('logout/', views.logout_view, name='logout')
 ]
--- TimLogin/main/views.py
+++ TimLogin/main/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from . forms import RegisterForm
+from django.contrib.auth import login, logout, authenticate
 
 def sign_up(request):
+    # if it is a POST request, we will fill register form with input data(in the displayed form)
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('/home')
+            return redirect('/home/')
+    # if it is a GET request, we will create a empty form and render it
     else:
         form = RegisterForm()
 
@@ -14,3 +18,6 @@ def sign_up(request):
 
 def home(request):
     return render(request, 'main/home.html')
+
+def logout_view(request):
+    logout(request)
