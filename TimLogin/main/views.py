0cfbb371021523b4cc353cc0785e34bbcf040a38
S0me2 <you@example.com>
2024-07-21 12:05:34 +0500

    3rd commit

--- TimLogin/main/views.py
+++ TimLogin/main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/home')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'registration/sign_up.html', {"form": form})
