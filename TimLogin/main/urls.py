237ac974b879c964b48548ca45051aae9804b077
S0me2 <you@example.com>
2024-07-22 11:55:01 +0500

    smth

--- TimLogin/main/urls.py
+++ TimLogin/main/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('sign-up/', views.sign_up, name='sign_up'),
     path('logout/', views.logout_view, name='logout'),
 
-    path('/password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
     path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
     path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
