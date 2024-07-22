1a576f6ab16b345011fb3eca65cec57cf79295b0
S0me2 <you@example.com>
2024-07-22 13:11:40 +0500

    pass change

--- TimLogin/main/urls.py
+++ TimLogin/main/urls.py
@@ -13,5 +13,9 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_conf.html'), name='password_reset_conf'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_compl.html'), name='password_reset_compl'),
 
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'), name="password_change"),
+    path('password_change/done/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change_do.html'), name="password_change_do"),
+
+
     path('test-template/', views.test_template, name='test-template'),
 ]
